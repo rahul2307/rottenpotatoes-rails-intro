86a66ce664cbac26790288ed52ffb61da77b727d
EC2 Default User <ec2-user@ip-172-31-82-221.ec2.internal>
2019-10-01 18:05:55 +0000

    test 2

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -18,7 +18,7 @@ class MoviesController < ApplicationController
     @movies = Movie.where(rating: @selected_ratings)
     @sort=params[:sort]
     if @sort
-      @movies = @movies.order(@sort)
+      @movies = @movies.where(rating: @selected_ratings).order(@sort)
     end
   end
 
